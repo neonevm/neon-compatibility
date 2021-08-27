efc2c47ed84581908acc969c55dcae5821dcd180
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-08-27 17:17:29 +0300

    truffle tests are done

--- tests/truffle/test_truffle.py
+++ tests/truffle/test_truffle.py
@@ -4,7 +4,7 @@ from src.helpers.common.constants import NETWORK_NAME
 
 CD_METACOIN = "cd Metacoin; pwd;"
 # ERROR_TAG_LATEST = "Error: Returned error: Invalid tag latest"
-ERROR_NO_ATTRIBUTE_ETH_ACCOUNTS = "Contracts have not been deployed to any network."
+ERROR_NO_ATTRIBUTE_ETH_ACCOUNTS = "Returned error: 'EthereumModel' object has no attribute 'eth_accounts'"
 ERROR_CONTRACTS_NOT_DEPLOYED = "Contracts have not been deployed to any network."
 CD_BACK = "; cd ..; pwd;"
 
@@ -30,4 +30,5 @@ def test_truffle_test():
     # truffle neonlabs ./test/metacoin.js
     actual_result = run_command_line(
         f"{CD_METACOIN} truffle {NETWORK_NAME} ./test/metacoin.js {CD_BACK}")
+    assert ERROR_CONTRACTS_NOT_DEPLOYED not in actual_result
     print(actual_result)
