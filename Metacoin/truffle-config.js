af198a928c49250e1cc53644cb4ee3b43bb44baf
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-09-16 14:59:55 +0300

    updates

--- Metacoin/truffle-config.js
+++ Metacoin/truffle-config.js
@@ -25,4 +25,24 @@ module.exports = {
       // timeoutBlocks: - if a transaction is not mined, keep waiting for this number of blocks (default is 50)
     },
   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.4", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
 };
