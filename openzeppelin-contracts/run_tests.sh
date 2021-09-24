acd248588db76b967eb3e5be332ba7a3fefbbe33
mich-master <minyas@yandex.ru>
2021-09-24 09:51:35 +0300

    checktest 3

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -1,1 +1,3 @@
-../node_modules/.bin/truffle --network neonlabs test
+../node_modules/.bin/truffle --network neonlabs test ./test/token/access/AccessControl.test.js
+../node_modules/.bin/truffle --network neonlabs test ./test/token/access/AccessControlEnumerable.test.js
+../node_modules/.bin/truffle --network neonlabs test ./test/token/access/Ownable.test.js
