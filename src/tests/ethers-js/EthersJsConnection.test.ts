367c80b8adcd1030307c371d322bc0ef0bb83bd1
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-09-26 09:22:24 +0300

    refactoring

--- src/tests/ethers-js/EthersJsConnection.test.ts
+++ src/tests/ethers-js/EthersJsConnection.test.ts
@@ -1,19 +1,25 @@
 import { suite, test } from "@testdeck/mocha";
 import { epic, feature } from "allure-decorators";
 import { expect } from "chai";
-import { ethers } from "ethers";
 import { Config } from "../../../config/default";
+import { ConnectionManager } from "../../helpers/ethers-js/ConnectionManager";
+import { DataRetrieval } from "../../helpers/ethers-js/DataRetrieval";
 
+const JsonRpcProviderShouldNotBeNull = "JsonRpcProvider should not be null";
+const BlockNumberShouldBeNonZero = "Block number should be non-zero";
 @suite("Ethers.js")
 class EthersJsConnectionTests {
   @epic("Ethers.js")
   @feature("Connection test")
   @test
-  public shouldConnectViaEthersJs() {
-    const provider = new ethers.providers.JsonRpcProvider(Config.url);
-    console.log(provider);
-    expect(provider, "JsonRpcProvider should not be null").to.not.be.null;
+  public async shouldConnectViaEthersJs() {
+    const provider = new ConnectionManager().connectToJsonRpc(Config.url);
+    console.log(`Provider: ${provider}`);
+    expect(provider, JsonRpcProviderShouldNotBeNull).to.not.be.null;
     expect(provider.connection.url).to.be.equal(Config.url);
+    const blockNumber = await new DataRetrieval().getBlockNumber(provider);
+    console.log(`Current block number = ${blockNumber}`);
+    expect(blockNumber, BlockNumberShouldBeNonZero).to.be.greaterThan(0);
   }
 
   public before() {}
