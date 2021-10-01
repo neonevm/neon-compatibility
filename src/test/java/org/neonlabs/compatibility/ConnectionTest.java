2af87ef9697b1ff75c865a2afdfda1d52419a39b
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-01 08:19:56 +0300

    JVM-related updates

---
+++ src/test/java/org/neonlabs/compatibility/ConnectionTest.java
@@ -0,0 +1,12 @@
+package org.neonlabs.compatibility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConnectionTest {
+    @Test
+    void probe() {
+        assertEquals(1, 1);
+    }
+}
