c05ff16dde2036ded8a74706904cebfd65105269
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-01 09:46:59 +0300

    Update run_tests.sh

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs migrate ======"
+echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs migrate
 # ../node_modules/.bin/truffle --network neonlabs migrate --reset --verbose-rpc
 
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
+echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js
+
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js ======"
+echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js
+
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js ======"
+echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js
