e7e9e3ceed23719f5c1397b656b379db9ef24649
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-03 18:27:36 +0300

    JavaScript/Typescript faucet is supported

--- config/default.ts
+++ config/default.ts
@@ -1,16 +1,18 @@
-import { config } from "dotenv";
+import { config } from 'dotenv';
 
 config();
 
 const getStringValue = (input: string | undefined) => {
-  return input === undefined ? "" : input;
+  return input === undefined ? '' : input;
 };
 
 export const Config = {
+  baseUrl: getStringValue(process.env.HTTP_URL).replace('/solana', ''),
   networkId: getStringValue(process.env.NETWORK_ID),
   url: getStringValue(process.env.HTTP_URL),
   addressFrom: getStringValue(process.env.ADDRESS_FROM),
   addressTo: getStringValue(process.env.ADDRESS_TO),
   disableConfirmation: getStringValue(process.env.DISDISABLE_CONFIRMATION),
+  faucetQuotient: parseInt(getStringValue(process.env.FAUCET_QUOTIENT)),
   privateKey: getStringValue(process.env.PRIVATE_KEY),
 };
