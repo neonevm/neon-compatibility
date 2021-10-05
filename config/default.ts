cda27b5bf60162e925b4915a21e0f6c0116340f7
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-05 10:40:52 +0300

    configuration updates

--- config/default.ts
+++ config/default.ts
@@ -7,12 +7,12 @@ const getStringValue = (input: string | undefined) => {
 };
 
 export const Config = {
-  baseUrl: getStringValue(process.env.HTTP_URL).replace('/solana', ''),
-  networkId: getStringValue(process.env.NETWORK_ID),
-  url: getStringValue(process.env.HTTP_URL),
   addressFrom: getStringValue(process.env.ADDRESS_FROM),
   addressTo: getStringValue(process.env.ADDRESS_TO),
+  baseUrl: getStringValue(process.env.HTTP_URL).replace('/solana', ''),
   disableConfirmation: getStringValue(process.env.DISDISABLE_CONFIRMATION),
   faucetQuotient: parseInt(getStringValue(process.env.FAUCET_QUOTIENT)),
+  networkId: getStringValue(process.env.NETWORK_ID),
   privateKey: getStringValue(process.env.PRIVATE_KEY),
+  url: getStringValue(process.env.HTTP_URL),
 };
