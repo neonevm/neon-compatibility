fb2174ae722f5a0f3bd90b70ee168d9d652b9cf1
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-12 01:07:24 +0300

    Update run_tests.sh

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -10,6 +10,9 @@ echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs migrate
 # ../node_modules/.bin/truffle --network neonlabs migrate --reset --verbose-rpc
 
+../node_modules/.bin/truffle --network neonlabs test
+exit
+
 echo "======================================================================"
 echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
 echo "======================================================================"
