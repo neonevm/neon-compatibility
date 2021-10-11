c80b7f71749b620a5e50fcc594f0e50b99159e65
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-11 20:24:58 +0300

    Update truffle-config.js

--- openzeppelin-contracts/truffle-config.js
+++ openzeppelin-contracts/truffle-config.js
@@ -97,9 +97,9 @@ module.exports = {
   mocha: {
     timeout: 1000000000,
     reporter: 'mocha-multi-reporters',
-    reporterOption: {
-      configFile: '../.mocharc.json'
-    }
+    // reporterOption: {
+    //   configFile: '../.mocharc.json'
+    // }
   },
 
   // Configure your compilers
