ae9a43ec70dd87da9b60ee95344faed2b59823bd
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-12 02:22:49 +0300

    Update run_tests.sh

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# for f in ./test; do
+#     ls basename $f
+# done
+
+find "$(pwd)" | grep test.js | ../node_modules/.bin/truffle --network neonlabs test $_
+exit
+
+
 echo "======================================================================"
 echo "======= ../node_modules/.bin/truffle --network neonlabs compile ======"
 echo "======================================================================"
@@ -10,8 +19,8 @@ echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs migrate
 # ../node_modules/.bin/truffle --network neonlabs migrate --reset --verbose-rpc
 
-../node_modules/.bin/truffle --network neonlabs test
-exit
+# ../node_modules/.bin/truffle --network neonlabs test
+# exit
 
 echo "======================================================================"
 echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
