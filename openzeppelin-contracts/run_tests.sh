264ffbb0f03ffb20e611180f62f396c6ddee9b7a
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-12 08:00:32 +0300

    Update run_tests.sh

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "======================================================================"
-echo "======= ../node_modules/.bin/truffle --network neonlabs compile ======"
-echo "======================================================================"
-../node_modules/.bin/truffle --network neonlabs compile
+# echo "======================================================================"
+# echo "======= ../node_modules/.bin/truffle --network neonlabs compile ======"
+# echo "======================================================================"
+# ../node_modules/.bin/truffle compile
 
-echo "======================================================================"
-echo "======= ../node_modules/.bin/truffle --network neonlabs migrate ======"
-echo "======================================================================"
-../node_modules/.bin/truffle --network neonlabs migrate
+# echo "======================================================================"
+# echo "======= ../node_modules/.bin/truffle --network neonlabs migrate ======"
+# echo "======================================================================"
+# ../node_modules/.bin/truffle --network neonlabs migrate
 # ../node_modules/.bin/truffle --network neonlabs migrate --reset --verbose-rpc
 
-find "$(pwd)" | grep test.js | ../node_modules/.bin/truffle --network neonlabs test $_
-exit
+# find "$(pwd)" | grep test.js | ../node_modules/.bin/truffle --network neonlabs test $_
+# exit
 # ../node_modules/.bin/truffle --network neonlabs test
 # exit
 
