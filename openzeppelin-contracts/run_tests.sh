748502060f58686b1b56e4d28e8c2bd924f7fea6
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-12 12:15:37 +0300

    Update run_tests.sh

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -12,22 +12,22 @@
 # ../node_modules/.bin/truffle --network neonlabs migrate
 # ../node_modules/.bin/truffle --network neonlabs migrate --reset --verbose-rpc
 
-find "$(pwd)" | grep test.js | ../node_modules/.bin/truffle --network neonlabs test $_
-exit
+# find "$(pwd)" | grep test.js | ../node_modules/.bin/truffle --network neonlabs test $_
+# exit
 
-# echo "======================================================================"
-# echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
-# echo "======================================================================"
-# ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
+echo "======================================================================"
+../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js
 
-# echo "======================================================================"
-# echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js ======"
-# echo "======================================================================"
-# ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js ======"
+echo "======================================================================"
+../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js
 
-# echo "======================================================================"
-# echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js ======"
-# echo "======================================================================"
-# ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js
+echo "======================================================================"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js ======"
+echo "======================================================================"
+../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js
 
 mv ./allure-results/ ../report/
