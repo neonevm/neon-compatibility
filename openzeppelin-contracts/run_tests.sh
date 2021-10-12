d623a52902a97e75e9a70c1e1ed82d24a980c67c
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-10-12 12:29:58 +0300

    Update run_tests.sh

--- openzeppelin-contracts/run_tests.sh
+++ openzeppelin-contracts/run_tests.sh
@@ -15,10 +15,25 @@
 # find "$(pwd)" | grep test.js | ../node_modules/.bin/truffle --network neonlabs test $_
 # exit
 
+# echo "======================================================================"
+# echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
+# echo "======================================================================"
+# ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js
+
+# echo "======================================================================"
+# echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js ======"
+# echo "======================================================================"
+# ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js
+
+# echo "======================================================================"
+# echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js ======"
+# echo "======================================================================"
+# ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js
+
 echo "======================================================================"
-echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js ======"
 echo "======================================================================"
-../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js
+../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js
 
 echo "======================================================================"
 echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js ======"
@@ -26,8 +41,8 @@ echo "======================================================================"
 ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControlEnumerable.test.js
 
 echo "======================================================================"
-echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js ======"
+echo "======= ../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js ======"
 echo "======================================================================"
-../node_modules/.bin/truffle --network neonlabs test ./test/access/Ownable.test.js
+../node_modules/.bin/truffle --network neonlabs test ./test/access/AccessControl.test.js
 
 mv ./allure-results/ ../report/
