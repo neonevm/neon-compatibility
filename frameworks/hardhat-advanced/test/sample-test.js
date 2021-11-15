760e6fb330ef48286b812402af1c6d5747623b30
Oleg <gigimon4ik@gmail.com>
2021-11-15 08:50:11 +0300

    Skip test

--- frameworks/hardhat-advanced/test/sample-test.js
+++ frameworks/hardhat-advanced/test/sample-test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Hardhat advanced: Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it.skip("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
