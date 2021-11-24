b36c0ce85934e34f1bf1f85a53367fe0afdc97ec
gigimon <gigimon4ik@gmail.com>
2021-11-24 18:01:33 +0300

    * use allure feature for all brownie tests

--- tests/brownie/conftest.py
+++ tests/brownie/conftest.py
@@ -7,6 +7,12 @@ from brownie import accounts, project
 from src.helpers.common.config import PROXY_URL, NETWORK_ID
 
 
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        mark = getattr(pytest.mark, 'allure_label').with_args("Brownie", label_type="feature" )
+        item.add_marker(mark)
+
+
 @pytest.fixture(scope="session", autouse=True)
 def configure_neon_network():
     CONFIG.networks['neon'] = {
--- tests/brownie/test_brownie.py
+++ tests/brownie/test_brownie.py
@@ -1,15 +1,6 @@
-import allure
-
 from brownie import network, accounts
 
-ADD_NETWORK = "brownie networks add Ethereum"
-DELETE_NETWORK = "brownie networks delete"
 
-FEATURE = 'brownie'
-
-
-@allure.feature(FEATURE)
-@allure.story("Connect brownie")
 def test_brownie_connect():
     network.connect('neon')
     assert network.is_connected()
@@ -18,10 +9,7 @@ def test_brownie_connect():
     assert network.gas_price() == 'auto'
 
 
-@allure.feature(FEATURE)
-@allure.story("Deploy contract")
 def test_brownie_deploy(brownie_project):
-    print(len(accounts))
     deployed_contract = brownie_project.Token.deploy(
         "Test deploy", "TST", 10, 10000000, {"from": accounts[0]}
     )
