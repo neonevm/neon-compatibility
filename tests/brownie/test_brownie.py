23a7fc4b37cfe21ea7f6ca222327c606abd0f3dc
Oleg <gigimon4ik@gmail.com>
2021-11-29 09:23:07 +0000

    Add brownie tests for compatibility (#132)
    * formatting
    
    * updates
    
    * Update .gitignore
    
    * .gitignore & clean-up
    
    * Delete hardhat.config.js.bak
    
    * .
    
    * Update .gitignore
    
    * Run brownie tests (connect to neon stand and deploy base contract)
    
    * run debug
    
    * run debug
    
    * run debug
    
    * run debug
    
    * fix path for brownie
    
    * * use brownie test project for test
    
    * * use allure feature for all brownie tests
    
    * * merge
    
    * * debug build
    
    * complete task
    
    * fix pylint
    
    * fix pr
    
    * remove unused github flow
    
    * fix run brownie tests (copy paste for brownie fixtures)
    
    * add link to bug
    
    * fix allure label
    
    * fix allure label
    
    Co-authored-by: apetrovskiy <apetrovskiy@hotmail.com>
    Co-authored-by: Alexander Petrovskiy <apetrovskiy2@hotmail.com>

--- tests/brownie/test_brownie.py
+++ tests/brownie/test_brownie.py
@@ -1,37 +1,16 @@
-import allure
-import pytest
-from src.helpers.common.config import CD_BACK, PROXY_URL
-from src.helpers.common.constants import NETWORK_NAME, Subfolder
-from src.helpers.shell.processes import run_command_line
-
-ADD_NETWORK = "brownie networks add Development"
-DELETE_NETWORK = "brownie networks delete"
-
-FEATURE = 'brownie'
+from brownie import network, accounts
 
 
-@pytest.fixture(autouse=True)
-def setup_and_teardown():
-    aaa = f"{Subfolder.CD_BROWNIE} {ADD_NETWORK} {NETWORK_NAME} \
-        host={PROXY_URL} {CD_BACK}"
-
-    print(aaa)
-    run_result = run_command_line(
-        f"{Subfolder.CD_BROWNIE} {ADD_NETWORK} {NETWORK_NAME} \
-            host={PROXY_URL} {CD_BACK}")
-    print(run_result)
-    yield
-    bbb = f"{Subfolder.CD_BROWNIE} {DELETE_NETWORK} {NETWORK_NAME} {CD_BACK}"
-    print(bbb)
-    run_result = run_command_line(
-        f"{Subfolder.CD_BROWNIE} {DELETE_NETWORK} {NETWORK_NAME} {CD_BACK}")
-    print(run_result)
+def test_brownie_connect():
+    network.connect('neon')
+    assert network.is_connected()
+    assert network.show_active() == 'neon'
+    assert network.gas_limit() == 'auto'
+    assert network.gas_price() == 'auto'
 
 
-@pytest.mark.skip(reason="now yet done")
-@allure.feature(FEATURE)
-def test_one():
-    aaa = f"{Subfolder.CD_BROWNIE} {ADD_NETWORK} {NETWORK_NAME} \
-        host={PROXY_URL} {CD_BACK}"
-
-    print(aaa)
+def test_brownie_deploy(brownie_project):
+    deployed_contract = brownie_project.Token.deploy(
+        "Test deploy", "TST", 10, 10000000, {"from": accounts[0]}
+    )
+    assert deployed_contract
