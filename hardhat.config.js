7f8b58a92d2985b95fdd1dc5b8beeb83dd8fb147
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-14 01:16:03 +0300

    Feature/186 increase timeout in openzeppelin contracts tests where possible (#188)
    * increase timeout in openzeppelin-contracts tests where possible
    
    * Update action.yml
    
    * Update action.yml
    
    * Update consecutive.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * Update hardhat.config.js
    
    * Update hardhat.config.js
    
    * Update action.yml
    
    * .

--- hardhat.config.js
+++ hardhat.config.js
@@ -36,7 +36,7 @@ const argv = require('yargs/yargs')()
     compiler: {
       alias: 'compileVersion',
       type: 'string',
-      default: '0.8.3'
+      default: '0.8.10'
     },
     coinmarketcap: {
       alias: 'coinmarketcapApiKey',
