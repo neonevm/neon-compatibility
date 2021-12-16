06d5fbe4c241406bedaa854efb7c32f18405425e
Vasiliy Zaznobin <vz@neonlabs.org>
2021-12-16 16:26:40 +0300

    Refactor paths

--- cli/run-oz-all-parallel.sh
+++ cli/run-oz-all-parallel.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-rm ./.env
-ln -s ./.env."$1" ./.env;
-source ./.env;
-rm -rf ../openzeppelin-contracts/allure-results/;
+rm ../.env
+ln -s ./.env."$1" ../.env;
+source ../.env;
 cd ../openzeppelin-contracts
+rm -rf ./allure-results/;
 clear;
 date;
 echo "$PROXY_URL"
+echo "$SOLANA_URL"
 export OZ_OUTPUT="oz_run_at_""$(date +%Y-%m-%d_%H.%M.%S)""_""$(echo $PROXY_URL | tr '\/' '_' | tr ' ' '_' | tr -d ':')"".txt"
-export OZ_TESTS=$(find test -name '*test.js' | sort )
+export OZ_TESTS=$(find test -name '*test.js' | sort | head -53 | tail -1 )
 echo "$OZ_TESTS" | tr '[[:space:]]' '\n' | cat -n
-time parallel -k "$2" ../cli/run-oz-test.sh $PROXY_URL {#} {%} {} ::: < <(echo "$OZ_TESTS" | tr '[[:space:]]' '\n') | tee "$OZ_OUTPUT"
+echo "pwd=$(pwd)"
+time parallel -k "$2" ./../cli/run-oz-test.sh $PROXY_URL $SOLANA_URL {#} {%} {} ::: < <(echo "$OZ_TESTS" | tr '[[:space:]]' '\n') | tee "$OZ_OUTPUT"
 
