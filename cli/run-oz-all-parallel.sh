a4c4f13383249f19fe4c24aafcd6611f38c33e8d
Vasiliy Zaznobin <vz@neonlabs.org>
2021-12-16 16:30:30 +0300

    Refactor paths

--- cli/run-oz-all-parallel.sh
+++ cli/run-oz-all-parallel.sh
@@ -10,8 +10,7 @@ date;
 echo "$PROXY_URL"
 echo "$SOLANA_URL"
 export OZ_OUTPUT="oz_run_at_""$(date +%Y-%m-%d_%H.%M.%S)""_""$(echo $PROXY_URL | tr '\/' '_' | tr ' ' '_' | tr -d ':')"".txt"
-export OZ_TESTS=$(find test -name '*test.js' | sort | head -53 | tail -1 )
+export OZ_TESTS=$(find test -name '*test.js' | sort )
 echo "$OZ_TESTS" | tr '[[:space:]]' '\n' | cat -n
-echo "pwd=$(pwd)"
 time parallel -k "$2" ./../cli/run-oz-test.sh $PROXY_URL $SOLANA_URL {#} {%} {} ::: < <(echo "$OZ_TESTS" | tr '[[:space:]]' '\n') | tee "$OZ_OUTPUT"
 
