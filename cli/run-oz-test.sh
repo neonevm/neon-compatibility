9c955717dade04fcca9862422265e2425eaffb86
Vasiliy Zaznobin <vz@neonlabs.org>
2021-12-16 13:31:13 +0300

    Added checking getHealth on start test

--- cli/run-oz-test.sh
+++ cli/run-oz-test.sh
@@ -14,6 +14,7 @@ NN="$2"
 DELAY="$3"
 TEST="$4"
 echo PROXY_URL="$PROXY_URL"
+echo SOLANA_URL="$SOLANA_URL"
 echo NN="$NN"
 echo DELAY="$DELAY"
 echo TEST="$TEST"
@@ -24,6 +25,8 @@ echo DD="$DD"
 curl -w '\n' -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":'$DD'}' $PROXY_URL
 curl -w '\n' -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"neon_cli_version","params":[],"id":'$DD'}' $PROXY_URL
 curl -w '\n' -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"neon_proxy_version","params":[],"id":'$DD'}' $PROXY_URL
+curl -w '\n' -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getHealth"}' "$SOLANA_URL"
+
 time npx hardhat test "$TEST" 2>&1
 sleep 1
 echo "=============================================================================="
