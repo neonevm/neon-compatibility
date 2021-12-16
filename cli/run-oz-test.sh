068baa84278f9897e411a0e08c184f613bc14650
Vasiliy Zaznobin <vz@neonlabs.org>
2021-12-16 13:35:43 +0300

    Use DD=$NN""$(date +%d%m%Y%H%M%S)

--- cli/run-oz-test.sh
+++ cli/run-oz-test.sh
@@ -20,7 +20,7 @@ echo DELAY="$DELAY"
 echo TEST="$TEST"
 sleep "$DELAY"
 date
-DD=$NN""$(date | tr -d '[:alpha:]: ')
+DD=$NN""$(date +%d%m%Y%H%M%S)
 echo DD="$DD"
 curl -w '\n' -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":'$DD'}' $PROXY_URL
 curl -w '\n' -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"neon_cli_version","params":[],"id":'$DD'}' $PROXY_URL
