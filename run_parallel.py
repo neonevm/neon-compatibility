1139391307486895f25db121ca3c804a558e60bf
Oleg <gigimon4ik@gmail.com>
2021-12-17 14:04:56 +0300

    Rewrite test workflow with agent and allure dirs (#197)
    * * experiments with matrix
    
    * * experiments with matrix
    
    * * experiments with matrix
    
    * * experiments with matrix
    
    * * small test fot reporter
    
    * * small test fot reporter
    
    * * small test fot reporter
    
    * * try parallel
    
    * * try parallel
    
    * * try parallel run with gnu parallel
    
    * * try parallel run with gnu parallel
    
    * * try parallel run with gnu parallel
    
    * * try parallel run with gnu parallel
    
    * * increase jobs
    
    * * increase jobs
    
    * * run on hosted agent
    
    * * simplify workflow
    
    * * run in agent
    
    * * run in agent
    
    * * use allure dirs
    
    * * use allure dirs
    
    * * use allure dirs
    
    * * merge develop
    
    * * fixes
    
    * * increase default job
    
    * * fix job number
    
    * * remove unused code
    
    * * fix network
    
    * * fix cleaner
    
    * * fix notification
    
    * * fix notification
    
    * * fix notification
    
    * * fix notification

---
+++ run_parallel.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+JOBS_NUMBER = int(os.environ.get("JOBS_NUMBER", 8))
+
+print(f"Jobs count: {JOBS_NUMBER}")
+tests = subprocess.check_output("find \"test\" -name '*.js'", shell=True).decode().splitlines()
+
+chunk_size = len(tests) / JOBS_NUMBER
+
+res = [tests[int(i*chunk_size):int((i+1)*chunk_size)] for i in range(JOBS_NUMBER)]
+
+out = []
+
+for r in res:
+    out.append(" ".join([str(c) for c in r]))
+
+command = " ".join(["\"%s\"" % c for c in out])
+
+subprocess.check_call("parallel --jobs %s ./rrrun.sh {} ::: %s" % (JOBS_NUMBER, command), shell=True)
