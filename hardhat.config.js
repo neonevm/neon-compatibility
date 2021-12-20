5b1de92f4a12124a977a63d4f5e52898e7224d9e
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-12-20 10:06:29 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -88,15 +88,17 @@ const account01 = web3.eth.accounts.create();
 process.env.ADDRESS_FROM = account01.address;
 process.env.PRIVATE_KEY = account01.privateKey;
 // (async () => await web3.eth.getBalance(account01.address))();
-await requestFaucet(account01.address, REQUEST_AMOUNT).then(
-  (data) => await getBalance(account01.address)
-);
+(async (address) =>
+  await requestFaucet(address, REQUEST_AMOUNT).then(
+    (data) => await getBalance(address)
+  ))(account01.address);
 const account02 = web3.eth.accounts.create();
 process.env.ADDRESS_TO = account02.address;
 // (async () => await web3.eth.getBalance(account02.address))();
-await requestFaucet(account02.address, REQUEST_AMOUNT).then(
-  (data) => await getBalance(account02.address)
-);
+(async (address) =>
+  await requestFaucet(address, REQUEST_AMOUNT).then(
+    (data) => await getBalance(address)
+  ))(account02.address);
 
 const privateKeys = Array.from(Array(ACCOUNTS_NUMBER), (_, x) => {
   const acc = web3.eth.accounts.create();
