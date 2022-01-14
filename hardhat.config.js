8a34c341535b145ec970799aec25fb89a76a5f96
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-01-14 19:44:42 +0700

    Wait for tokens to come from the faucet before the test running.

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,21 +22,24 @@ const getBooleanValue = (input) => {
     : false;
 };
 
+const getArrayValue = (input) => {
+  return input === undefined ? [] : String(input).split(',')
+}
+
 const Config = {
-  baseUrl: getStringValue(process.env.PROXY_URL).replace('/solana', ''),
-  addressFrom: getStringValue(process.env.ADDRESS_FROM),
-  addressTo: getStringValue(process.env.ADDRESS_TO),
-  privateKey: getStringValue(process.env.PRIVATE_KEY),
-  requestAmount: Number.parseInt(getStringValue(process.env.REQUEST_AMOUNT)),
-  faucetUrl: getStringValue(process.env.FAUCET_URL),
-  useFaucet: getBooleanValue(process.env.USE_FAUCET),
   network: getStringValue(process.env.NETWORK_NAME),
   networkId: getStringValue(process.env.NETWORK_ID),
   proxyUrl: getStringValue(process.env.PROXY_URL),
-  solanaExplorer: getStringValue(process.env.SOLANA_EXPLORER),
-  solanaUrl: getStringValue(process.env.SOLANA_URL),
-  usersNumber: Number.parseInt(getStringValue(process.env.USERS_NUMBER))
+  privateKeys: getArrayValue(process.env.PRIVATE_KEYS)
 };
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(Config.proxyUrl, 3000000)
+);
+
+process.env.PRIVATE_KEY = Config.privateKeys[0];
+process.env.ADDRESS_FROM = web3.eth.accounts.privateKeyToAccount(Config.privateKeys[0]).address;
+process.env.ADDRESS_TO = web3.eth.accounts.privateKeyToAccount(Config.privateKeys[1]).address;
 
 const fs = require('fs');
 const path = require('path');
@@ -87,89 +90,6 @@ const withOptimizations =
   argv.enableGasReport || argv.compileMode === 'production';
 
 
-if (Config.useFaucet){
-  console.log(`Use faucet: ${Config.faucetUrl}`)
-} else {
-  console.log(`Skipping faucet requests: USE_FAUCET set to false`);
-}
-
-const requestFaucet = async (wallet, amount) => {
-  if (!Config.useFaucet) {
-    return;
-  }
-  const data = { amount: amount, wallet: wallet };
-  if (Config.faucetUrl.length === 0) {
-    console.log('Unable to request faucet: FAUCET_URL is empty');
-    return;
-  }
-  console.log(`Request '${data.amount}' tokens for wallet ${data.wallet} from faucet`);
-    try {
-      const resp = await axios.post(Config.faucetUrl, data);
-      console.log(`Faucet response code: ${resp.status}`);
-    } catch (err) {
-      console.log(`Failed to send request to faucet: ${err.response.status} ${err.response.data}`);
-    }
-};
-
-const getBalance = async (address) => {
-    try {
-      return await web3.eth.getBalance(address);
-    } catch (e) {
-      console.log(`Failed to get balance for wallet ${address}: ${e}`);
-    }
-}
-
-const requestFaucetAndGetBalance = async (address, amount) => {
-  await requestFaucet(address, amount);
-  await getBalance(address);
-};
-
-const web3 = new Web3(
-  new Web3.providers.HttpProvider(Config.proxyUrl, 3000000)
-);
-const account01 = web3.eth.accounts.create();
-process.env.ADDRESS_FROM = account01.address;
-process.env.PRIVATE_KEY = account01.privateKey;
-(async (address, amount) => requestFaucetAndGetBalance(address, amount))(
-  account01.address,
-  Config.requestAmount
-);
-const account02 = web3.eth.accounts.create();
-process.env.ADDRESS_TO = account02.address;
-(async (address, amount) => requestFaucetAndGetBalance(address, amount))(
-  account02.address,
-  Config.requestAmount
-);
-
-const privateKeys = Array.from(Array(Config.usersNumber), (_, x) => {
-  const acc = web3.eth.accounts.create();
-  (async (address, amount) => requestFaucetAndGetBalance(address, amount))(
-    acc.address,
-    Config.requestAmount
-  );
-  return acc.privateKey;
-});
-privateKeys.unshift(process.env.PRIVATE_KEY);
-
-console.log(
-  '========================== Reading Hardhat config ============================='
-);
-(async (addressFrom, addressTo) => {
-  console.log(
-    `address from = ${addressFrom} balance=`,
-    await getBalance(addressFrom)
-  );
-  console.log(
-    `address to = ${addressTo}  balance=`,
-    await getBalance(addressTo)
-  );
-  console.log(`main private key = ${process.env.PRIVATE_KEY}`);
-  console.log(`account keys = ${privateKeys}`);
-  console.log(
-    '=============================================================================='
-  );
-})(process.env.ADDRESS_FROM, process.env.ADDRESS_TO);
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -191,7 +111,7 @@ module.exports = {
     },
     neonlabs: {
       url: Config.proxyUrl,
-      accounts: privateKeys,
+      accounts: Config.privateKeys,
       from: process.env.ADDRESS_FROM,
       to: process.env.ADDRESS_TO,
       network_id: parseInt(Config.networkId),
