d3b5e369e147fd40ceae62b383ebf3becdc4202b
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-02 16:12:40 +0000

    correct fix_allure.py to make it compatible with lower python version

--- fix_allure.py
+++ fix_allure.py
@@ -4,7 +4,7 @@ import pathlib
 
 openzeppelin_reports = pathlib.Path('openzeppelin-contracts/report/allure-results')
 
-for res_file in openzeppelin_reports.glob('*-result.json'):
+for res_file in map(str, openzeppelin_reports.glob('*-result.json')):
     with open(res_file, 'r+') as f:
         report = json.load(f)
     report["labels"].append({
