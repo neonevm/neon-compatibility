d55ad5631176c0a48da7fe0ee64d1fd64f89ecaa
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-03 04:47:08 +0000

    spit and polish

--- parallel_report.py
+++ parallel_report.py
@@ -1,35 +1,49 @@
 import re
-import subprocess
+import os
+import glob
+from typing import Dict, Tuple, List
 
 
-test_report = {
-    "passing": 0,
-    "pending": 0,
-    "failing": 0
-}
 
-skipped_files = []
+def process_test_suite_results() -> Tuple[Dict[str, int], List[str]]:
 
-stdout_files = subprocess.check_output('find openzeppelin-contracts/pout -name "stdout" | sort', shell=True).decode().splitlines()
+    test_report  = {
+        "passing": 0,
+        "pending": 0,
+        "failing": 0
+    }
 
-for stdout in stdout_files:
-    with open(stdout, 'r+', encoding="utf8") as f:
-        rep = f.read()
-        result = re.findall(r"(\d+) (passing|pending|failing)", rep)
-        if not result:
-            skipped_files.append(stdout)
-        for count in result:
-            test_report[count[1]] += int(count[0])
+    skipped_files = []
 
-print("Count of files: {}\n".format(str(len(stdout_files))))
-print("Summarize result:\n")
-for state in test_report:
-    print("    {} - {}".format(state.capitalize(), test_report[state]))
-print("\nTotal tests - {:d}\n".format(sum(test_report.values())))
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    stdout_files = glob.glob(script_path + "/**/stdout", recursive=True)
+    print("Count of `stdout` files found: {}. Processing ...\n".format(str(len(stdout_files))))
 
-print("Test files without test result - {}:\n".format(len(skipped_files)))
+    for stdout in stdout_files:
+        with open(stdout, 'r+', encoding="utf8") as f:
+            rep = f.read()
+            result = re.findall(r"(\d+) (passing|pending|failing)", rep)
+            if not result:
+                skipped_files.append(stdout)
+            for count in result:
+                test_report[count[1]] += int(count[0])
+    return test_report, skipped_files
 
-for f in skipped_files:
-    test_file_name = f.split("/", 3)[3].rsplit("/", 1)[0].replace("_", "")
-    print(format("    {}", test_file_name))
+
+def print_test_suite_results(test_report: Dict[str, int], skipped_files: List[str]):
+
+    print("Summarize result:\n")
+    for state in test_report:
+        print("    {} - {}".format(state.capitalize(), test_report[state]))
+    print("\nTotal tests - {:d}\n".format(sum(test_report.values())))
+
+    print("Test files without test result - {}:\n".format(len(skipped_files)))
+
+    for f in skipped_files:
+        test_file_name = f.split("/", 3)[3].rsplit("/", 1)[0].replace("_", "")
+        print(format("    {}", test_file_name))
+     
+
+test_report, skipped_files = process_test_suite_results()
+print_test_suite_results(test_report, skipped_files)
 
--- run-test.sh
+++ run-test.sh
@@ -2,4 +2,5 @@
 
 npx hardhat compile
 find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER} --results pout 'npx hardhat test {}'
-python3 run_parallel.py
+
+python3 /opt/parallel_report.py
