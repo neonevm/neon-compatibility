b382427945358ab1bde350a8ad2a135506b18cf2
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-05 10:20:24 +0000

    Make JOBS_NUMBER environment optional

--- run-test.sh
+++ run-test.sh
@@ -2,7 +2,9 @@
 
 npx hardhat compile #TODO: move it to Dockerfile after the funding functionality will be dropped from the hardhat config
 
-JOBS_NUMBER=$(cat /proc/cpuinfo | grep processor | wc -l)
+PROC_COUNT=$(nproc)
+JOBS_NUMBER=${JOBS_NUMBER:-$PROC_COUNT}
+echo "Open Zeppeling tests are processing. JOBS_NUMBER: ${JOBS_NUMBER}"
 find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER} --results pout 'npx hardhat test {}'
 
 python3 /opt/parallel_report.py
