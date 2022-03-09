935f4f5b2f1acd66f52e987c4caea1c84a2358f7
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-09 17:21:38 +0500

    Fake run test

--- run-test.sh
+++ run-test.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-npx hardhat compile #TODO: move it to Dockerfile after the funding functionality will be dropped from the hardhat config
+cat << EOF
+Summarize result:
+ 
+    Failing - 33
+    Pending - 121
+    Passing - 300
 
-PROC_COUNT=$(nproc)
-JOBS_NUMBER=${JOBS_NUMBER:-$PROC_COUNT}
-echo "Open Zeppeling tests are processing. JOBS_NUMBER: ${JOBS_NUMBER}"
-find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER} --results pout 'npx hardhat test {}'
+Total tests - 1088
 
-python3 /opt/parallel_report.py
-python3 /opt/fix_allure.py
-echo "The allure-results archive is creating at: /opt/allure-reports.tar"
-tar -cf /opt/allure-reports.tar -C /opt/openzeppelin-contracts/report/allure-results/ .
+Test files without test result - 0:
+
+Fix allure results: 0
+The allure-results archive is creating at: /opt/allure-reports.tar
+    Pending - 121
+    Passing - 934
+
+EOF
+
+
+#
+#
+#npx hardhat compile #TODO: move it to Dockerfile after the funding functionality will be dropped from the hardhat config
+#
+#PROC_COUNT=$(nproc)
+#JOBS_NUMBER=${JOBS_NUMBER:-$PROC_COUNT}
+#echo "Open Zeppeling tests are processing. JOBS_NUMBER: ${JOBS_NUMBER}"
+#find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER} --results pout 'npx hardhat test {}'
+#
+#python3 /opt/parallel_report.py
+#python3 /opt/fix_allure.py
+#echo "The allure-results archive is creating at: /opt/allure-reports.tar"
+#tar -cf /opt/allure-reports.tar -C /opt/openzeppelin-contracts/report/allure-results/ .
