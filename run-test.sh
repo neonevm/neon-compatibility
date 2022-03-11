2b646b056e87200be27b8be24f737e1426e5e5b8
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-11 13:31:56 +0500

    correct run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -5,7 +5,7 @@ JOBS_NUMBER=${JOBS_NUMBER:-$PROC_COUNT}
 echo "Open Zeppeling tests are processing. JOBS_NUMBER: ${JOBS_NUMBER}"
 
 for i in $(seq ${JOBS_NUMBER} ); do export LIST_$i=$(node ./get_private_keys.js); done;
-find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER}--results pout "env PRIVATE_KEYS=\$LIST_{%} npx hardhat test {}"
+find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER} --results pout "env PRIVATE_KEYS=\$LIST_{%} npx hardhat test {}"
 
 python3 /opt/parallel_report.py
 python3 /opt/fix_allure.py
