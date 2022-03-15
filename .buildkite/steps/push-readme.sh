40aba190b13d33a7371b6198066e51353875af65
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-15 20:00:42 +0400

    push-readme.sh

--- .buildkite/steps/push-readme.sh
+++ .buildkite/steps/push-readme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -t -v $(pwd)/full_test_suite/:/full_test_suite -e DOCKER_USER='rozhkovdmitrii' -e DOCKER_PASS='7d5bcd02' \
+docker run --rm -t -v $(pwd)/full_test_suite/:/full_test_suite -e DOCKER_USER='${DHUBU}' -e DOCKER_PASS='${DHUBP}' \
       -e PUSHRM_PROVIDER=dockerhub -e PUSHRM_FILE=/full_test_suite/README.md \
       -e PUSHRM_SHORT='Full test suite neonlabsorg docker image is aimed to facilitate the full testing of Neon EVM' \
       -e PUSHRM_TARGET=neonlabsorg/full_test_suite -e PUSHRM_DEBUG=1 chko/docker-pushrm:1
