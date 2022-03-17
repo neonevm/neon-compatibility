318cd49fcf889167e2d35114a0ed5be1247cb6b1
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-17 14:46:02 +0400

    Get rid of quotes

--- .buildkite/steps/push-readme.sh
+++ .buildkite/steps/push-readme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker login -u $DHUBU -p $DHUBP
 docker run --rm -t -v $(pwd)/:/full_test_suite -e DOCKER_USER="${DHUBU}" -e DOCKER_PASS="${DHUBP}" \
       -e PUSHRM_PROVIDER=dockerhub -e PUSHRM_FILE=/full_test_suite/DOCKERHUB.md \
       -e PUSHRM_SHORT='Full test suite neonlabsorg docker image is aimed to facilitate the full testing of Neon EVM' \
