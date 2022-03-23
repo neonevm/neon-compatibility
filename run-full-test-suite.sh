acfb583856fc6f69c9bdf3a3723585cca0d14d66
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-03-23 22:57:56 +0400

    Add logs for generating private keys

--- run-full-test-suite.sh
+++ run-full-test-suite.sh
@@ -5,7 +5,12 @@ JOBS_NUMBER=${FTS_JOBS_NUMBER:-$PROC_COUNT}
 USERS_NUMBER=${FTS_JOBS_NUMBER}
 echo "Open Zeppeling tests are processing. JOBS_NUMBER: ${JOBS_NUMBER}"
 
-for i in $(seq ${JOBS_NUMBER} ); do export LIST_$i=$(node ./get_private_keys.js); done;
+for i in $(seq ${JOBS_NUMBER} ); do
+  VAR_NAME=LIST_$i
+  export ${VAR_NAME}=$(node ./get_private_keys.js);
+  echo "Private keys generated for: ${VAR_NAME} are: ${!VAR_NAME}"
+done;
+
 find test -name "*.test.js" | sort | parallel -k --jobs ${JOBS_NUMBER} --results pout "env PRIVATE_KEYS=\$LIST_{%} npx hardhat test {}"
 
 python3 /opt/parallel_report.py
